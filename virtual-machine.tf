d8fc5ab5a57dcf2208dec1711cb54557f598e7f3
ITEnthusiast0821 <92159600+ITEnthusiast0821@users.noreply.github.com>
2023-10-19 07:52:13 -0500

    Update virtual-machine.tf

--- virtual-machine.tf
+++ virtual-machine.tf
@@ -10,10 +10,10 @@ terraform {
 # There are currently no configuration options for the provider itself.
 
 resource "virtualbox_vm" "node" {
-  count     = 2
+  count     = 1
   name      = format("node-%02d", count.index + 1)
   image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
-  cpus      = 2
+  cpus      = 1
   memory    = "512 mib"
 #  user_data = file("${path.module}/user_data")
 
